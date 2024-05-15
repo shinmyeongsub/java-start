03aa29ae93622f7d8c85b6c64b4d5d597d7d8ab8
MyeongSeop Shin <83411963+shinmyeongsub@users.noreply.github.com>
2024-05-15 22:59:43 +0900

    Feature : Comment (주석)

---
+++ src/CommentJava.java
@@ -0,0 +1,12 @@
+public class CommentJava {
+    // psvm
+    public static void main(String[] args) {
+        System.out.println("hello java1"); // hello java1 출력
+        System.out.println("hello java2"); // hello java2 출력
+
+        /*
+        System.out.println("hello java3");
+        System.out.println("hello java4");
+         */
+    }
+}
